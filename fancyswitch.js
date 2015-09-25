d5624e9f63444a0b1a5ecb47ee6df3601817dbfe
Markus Ahrweiler <markus.ahrweiler@web.de>
2015-09-25 23:42:07 +0200

    Update with Template

--- fancyswitch.js
+++ fancyswitch.js
@@ -1,4 +1,4 @@
-(function ( window, $ ) {
+(function( window, $ ) {
 
     Fancy.require( {
         jQuery: false,
@@ -9,19 +9,30 @@
         logged  = false;
 
 
-    var template     = '<div class="FancySwitch-item">#name#<div class="FancySwitch-up">$upText$</div><div class="FancySwitch-down">$downText$</div></div>',
-        templateIcon = '<div class="FancySwitch-item">#name#<div class="FancySwitch-up $upClass$"></div><div class="FancySwitch-down $downClass$"></div></div>',
-        templateDrag = '<div class="FancySwitch-item">#name#</div>';
+    var template     = '<div class="FancySwitch-item">{{name}}<div class="FancySwitch-up">$upText$</div><div class="FancySwitch-down">$downText$</div></div>',
+        templateIcon = '<div class="FancySwitch-item">{{name}}<div class="FancySwitch-up $upClass$"></div><div class="FancySwitch-down $downClass$"></div></div>',
+        templateDrag = '<div class="FancySwitch-item">{{name}}</div>';
 
     function preventSelect( el ) {
         return el.on( "selectstart", false ).attr( 'unselectable', "on" ).css( "userSelect", "none" );
     }
 
+    function change( SELF, index, index2 ) {
+
+        if( Fancy.template ) {
+            SELF.templates.forEach( function( it ) {
+                it.update();
+            } );
+        }
+        SELF.settings.onChange.call( SELF, index, index2 );
+    }
 
     function swapArrayElements( arr, indexA, indexB ) {
-        var temp      = arr[ indexA ];
-        arr[ indexA ] = arr[ indexB ];
-        arr[ indexB ] = temp;
+        var temp = arr[ indexA ];
+        arr[ indexA ]        = arr[ indexB ];
+        arr[ indexB ]        = temp;
+        arr[ indexA ].$index = indexA;
+        arr[ indexB ].$index = indexB;
     }
 
     function FancySwitch( element, settings, list ) {
@@ -30,7 +41,7 @@
         SELF.settings = $.extend( {}, Fancy.settings[ NAME ], settings );
         delete SELF.settings.templates;
         SELF.visible  = false;
-        if ( !logged ) {
+        if( !logged ) {
             logged = true;
             Fancy.version( SELF );
         }
@@ -40,22 +51,24 @@
         SELF.name      = NAME;
         SELF.items     = SELF.element.find( SELF.settings.itemSelector );
         SELF.animating = false;
+        if( Fancy.template )
+            SELF.templates = [];
 
-        if ( !SELF.list.length ) {
-            SELF.element.on( "DOMNodeInserted." + NAME + "DOMNodeRemoved." + NAME, function () {
-                if ( SELF.element.find( SELF.settings.itemSelector ).length != SELF.items.length )
+        if( !SELF.list.length ) {
+            SELF.element.on( "DOMNodeInserted." + NAME + "DOMNodeRemoved." + NAME, function() {
+                if( SELF.element.find( SELF.settings.itemSelector ).length != SELF.items.length )
                     SELF.update();
             } );
         } else {
-            if ( !SELF.settings.template ) {
-                if ( SELF.settings.drag ) {
+            if( !SELF.settings.template ) {
+                if( SELF.settings.drag ) {
                     SELF.settings.template = templateDrag;
-                } else if ( SELF.settings.upText && SELF.settings.downText ) {
+                } else if( SELF.settings.upText && SELF.settings.downText ) {
                     SELF.settings.template = template;
-                } else if ( SELF.settings.upClass && SELF.settings.downClass ) {
+                } else if( SELF.settings.upClass && SELF.settings.downClass ) {
                     SELF.settings.template = templateIcon;
                 }
-                if ( !SELF.settings.template ) {
+                if( !SELF.settings.template ) {
                     throw "Error: could not define template";
                 }
             }
@@ -63,7 +76,7 @@
         SELF.element.addClass( NAME + "-element" );
 
 
-        if ( SELF.settings.drag ) {
+        if( SELF.settings.drag ) {
             SELF.element.addClass( NAME + "-draggable" );
         }
 
@@ -76,81 +89,83 @@
     FancySwitch.api = FancySwitch.prototype = {};
     FancySwitch.api.version = VERSION;
     FancySwitch.api.name    = NAME;
-    FancySwitch.api.up      = function ( item, animated ) {
+    FancySwitch.api.up      = function( item, animated ) {
         var SELF = this;
         var prev = item.prev();
-        if ( prev.length ) {
+        if( prev.length ) {
             var index = SELF.items.index( item );
-            if ( SELF.list.length ) {
+            if( SELF.list.length ) {
                 swapArrayElements( SELF.list, index, index - 1 );
             }
-            if ( animated === false || SELF.settings.animated === false ) {
+            if( animated === false || SELF.settings.animated === false ) {
                 item.insertBefore( prev );
                 SELF.items = SELF.element.find( SELF.settings.itemSelector );
-                SELF.settings.onChange.call( SELF, index, index - 1 );
-            } else if ( !SELF.animating ) {
+                change( SELF, index, index - 1 );
+            } else if( !SELF.animating ) {
                 SELF.animating = !SELF.animating;
                 item.css( "position", "relative" ).animate( {
                     top: "-" + prev.height() + "px"
-                }, 500, function () {
+                }, 500, function() {
                     item.css( "position", item.data( "position" ) ).css( "top", item.data( "top" ) );
                 } );
                 prev.css( "position", "relative" ).animate( {
                     top: "+" + item.height() + "px"
-                }, 500, function () {
+                }, 500, function() {
                     prev.css( "position", prev.data( "position" ) ).css( "top", prev.data( "top" ) );
                     item.insertBefore( prev );
                     SELF.animating = !SELF.animating;
                     SELF.items     = SELF.element.find( SELF.settings.itemSelector );
-                    SELF.settings.onChange.call( SELF );
+                    change( SELF, index, index - 1 );
                 } );
             }
         }
     };
-    FancySwitch.api.down    = function ( item, animated ) {
+    FancySwitch.api.down    = function( item, animated ) {
         var SELF = this;
         var next = item.next();
-        if ( item.next().length ) {
+        if( item.next().length ) {
             var index = SELF.items.index( item );
-            if ( SELF.list.length ) {
+            if( SELF.list.length ) {
                 swapArrayElements( SELF.list, index, index + 1 );
             }
-            if ( animated === false || SELF.settings.animated === false ) {
+            if( animated === false || SELF.settings.animated === false ) {
                 item.insertAfter( next );
                 SELF.items = SELF.element.find( SELF.settings.itemSelector );
-                SELF.settings.onChange.call( SELF, index, index + 1 );
-            } else if ( !SELF.animating ) {
+                change( SELF, index, index + 1 );
+            } else if( !SELF.animating ) {
                 SELF.animating = !SELF.animating;
                 item.css( "position", "relative" ).animate( {
                     top: "+" + next.height() + "px"
-                }, 500, function () {
+                }, 500, function() {
                     item.css( "position", item.data( "position" ) ).css( "top", item.data( "top" ) );
                 } );
                 next.css( "position", "relative" ).animate( {
                     top: "-" + item.height() + "px"
-                }, 500, function () {
+                }, 500, function() {
                     next.css( "position", next.data( "position" ) ).css( "top", next.data( "top" ) );
                     item.insertAfter( next );
                     SELF.animating = !SELF.animating;
+
+                    swapArrayElements( SELF.items, index, index + 1 );
                     SELF.items     = SELF.element.find( SELF.settings.itemSelector );
-                    SELF.settings.onChange.call( SELF );
+                    change( SELF, index, index + 1 );
                 } );
             }
         }
     };
-    FancySwitch.api.update  = function () {
-        if ( !this.list.length )
+    FancySwitch.api.update  = function() {
+        if( !this.list.length )
             this.items = this.element.find( this.settings.itemSelector );
         this.process();
     };
-    FancySwitch.api.process = function () {
+    FancySwitch.api.process = function() {
 
         function processTemplateItem( item ) {
 
             item.data( "processed", true ).data( "position", item.css( "position" ) === "static" ? "" : item.css( "position" ) ).data( "top", item.css( "top" ) === "auto" ? "" : item.css( "top" ) );
             var $el;
-            if ( !item.find( SELF.settings.upSelector ).length && !SELF.settings.drag ) {
-                if ( SELF.settings.upSelector.indexOf( "." ) === 0 ) {
+            if( !item.find( SELF.settings.upSelector ).length && !SELF.settings.drag ) {
+                if( SELF.settings.upSelector.indexOf( "." ) === 0 ) {
                     $el = $( "<div/>", {
                         "class": SELF.settings.upSelector.substr( 1 )
                     } );
@@ -160,11 +175,11 @@
                 $el.addClass( SELF.settings.upClass ).html( SELF.settings.upText );
 
                 item.append( $el );
-            } else if ( SELF.settings.drag ) {
+            } else if( SELF.settings.drag ) {
                 item.find( SELF.settings.upSelector ).remove();
             }
-            if ( !item.find( SELF.settings.downSelector ).length && !SELF.settings.drag ) {
-                if ( SELF.settings.downSelector.indexOf( "." ) === 0 ) {
+            if( !item.find( SELF.settings.downSelector ).length && !SELF.settings.drag ) {
+                if( SELF.settings.downSelector.indexOf( "." ) === 0 ) {
                     $el = $( "<div/>", {
                         "class": SELF.settings.downSelector.substr( 1 )
                     } );
@@ -174,32 +189,32 @@
                 $el.addClass( SELF.settings.downClass ).html( SELF.settings.downText );
 
                 item.append( $el );
-            } else if ( SELF.settings.drag ) {
+            } else if( SELF.settings.drag ) {
                 item.find( SELF.settings.downSelector ).remove();
             }
 
-            if ( !SELF.settings.drag ) {
-                preventSelect( item.find( SELF.settings.upSelector ) ).click( function () {
+            if( !SELF.settings.drag ) {
+                preventSelect( item.find( SELF.settings.upSelector ) ).click( function() {
                     SELF.up( item );
                 } );
 
-                preventSelect( item.find( SELF.settings.downSelector ) ).click( function () {
+                preventSelect( item.find( SELF.settings.downSelector ) ).click( function() {
                     SELF.down( item );
                 } );
             } else {
                 item.addClass( NAME + "-draggable-item" );
                 var offset,
                     handler = SELF.settings.handler ? item.find( SELF.settings.handler ) : item
-                preventSelect( handler ).on( "mousedown." + NAME, function ( e ) {
+                preventSelect( handler ).on( "mousedown." + NAME, function( e ) {
                     var clone;
 
-                    if ( e.which === 1 ) {
+                    if( e.which === 1 ) {
                         offset = {
                             x: e.pageX - item.offset().left,
                             y: e.pageY - item.offset().top
                         };
-                        $( document ).on( "mousemove." + NAME, function ( event ) {
-                            if ( !clone ) {
+                        $( document ).on( "mousemove." + NAME, function( event ) {
+                            if( !clone ) {
                                 clone = item.clone();
                                 $( "body" ).append( clone );
                                 clone.css( {
@@ -212,22 +227,22 @@
                                 top : Math.min( SELF.items.last().position().top, Math.max( SELF.items.first().position().top, event.pageY - offset.y ) ),
                                 left: item.offset().left
                             } );
-                            if ( clone.position().top > item.position().top + (item.height() / 3 * 2) ) {
+                            if( clone.position().top > item.position().top + (item.height() / 3 * 2) ) {
                                 SELF.down( item, false );
-                            } else if ( clone.position().top + (item.height() / 3 * 2) < item.position().top ) {
+                            } else if( clone.position().top + (item.height() / 3 * 2) < item.position().top ) {
                                 SELF.up( item, false );
                             }
 
-                        } ).on( "mouseup." + NAME, function ( event ) {
-                            if ( event.which === 1 && clone ) {
+                        } ).on( "mouseup." + NAME, function( event ) {
+                            if( event.which === 1 && clone ) {
                                 clone.animate( {
                                     top: item.position().top
-                                }, 300, function () {
+                                }, 300, function() {
                                     clone.remove();
                                     clone = null;
                                 } );
                                 $( document ).off( "." + NAME );
-                            } else if ( event.which === 1 ) {
+                            } else if( event.which === 1 ) {
                                 $( document ).off( "." + NAME );
                             }
                         } );
@@ -237,29 +252,38 @@
         }
 
         var SELF = this;
-        if ( SELF.list.length ) {
-            SELF.list.forEach( function ( it, i ) {
-                var item   = $( SELF.settings.template.replace( /#([^#]*)#/g, function ( match, $1 ) {
-                    return it[ $1 ];
-                } ).replace( /\$([^$]*)\$/g, function ( match, $1 ) {
+        if( SELF.list.length ) {
+            SELF.list.forEach( function( it, i ) {
+                it.$index    = i;
+                var item;
+                var template = SELF.settings.template.replace( /\$([^$}]*)\$/g, function( match, $1 ) {
                     return SELF.settings[ $1 ];
-                } ) );
+                } );
+                if( !Fancy.template ) {
+                    template = template.replace( /\{\{([^\{}]*)\}\}/g, function( match, $1 ) {
+                        return it[ $1 ];
+                    } );
+                }
+                item       = $( template );
+                if( Fancy.template ) {
+                    SELF.templates.push( Fancy( item ).template( { scope: it } ).compile() );
+                }
                 SELF.element.append( item );
                 SELF.items = SELF.items.add( item );
                 processTemplateItem( SELF.settings.handler ? item.find( SELF.settings.handler ) : item );
             } );
         } else {
-            SELF.items.each( function () {
+            SELF.items.each( function() {
                 var item = $( this );
-                if ( !item.data( "processed" ) ) {
+                if( !item.data( "processed" ) ) {
                     processTemplateItem( item );
                 }
             } );
         }
     };
-    FancySwitch.api.destroy = function () {
+    FancySwitch.api.destroy = function() {
         var SELF = this;
-        SELF.items.each( function () {
+        SELF.items.each( function() {
             var item = $( this );
             item.data( "processed", undefined ).data( "position", undefined ).data( "top", undefined );
             item.off( "." + NAME );
@@ -278,14 +302,15 @@
         downClass   : false,
         upText      : "Up",
         downText    : "Down",
+        template    : false,
         templates   : { "buttonText": template, "buttonClass": templateIcon, "drag": templateDrag },
-        onChange    : function () {},
+        onChange    : function() {},
         handler     : false
     };
 
     Fancy.switch     = VERSION;
-    Fancy.api.switch = function ( settings, list ) {
-        return this.set( NAME, function ( el ) {
+    Fancy.api.switch = function( settings, list ) {
+        return this.set( NAME, function( el ) {
             return new FancySwitch( el, settings, list );
         } );
     };
